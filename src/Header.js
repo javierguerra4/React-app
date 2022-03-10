efc098b3245950e4ca62164e87189f9656b5918a
Javier Guerra <javier.j.guerra4@gmail.com>
2022-03-10 14:41:22 -0600

    imported image in Header.js

--- src/Header.js
+++ src/Header.js
@@ -1,10 +1,10 @@
 import React from "react";
-
+import logo from "./images/react_logo.png";
 export default function Header() {
   return (
     <header className="header-items">
       <nav className="nav">
-        <img className="logo" src="images/react_logo.png" />
+        <img alt="Logo" src={logo} />
         <ul className="nav-items">
           <li>Pricing</li>
           <li>About</li>
--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import Header from "./Header";
 import Footer from "./Footer";
 import MainContent from "./MainContent";
 
-function Page() {
+function App() {
   return (
     <div>
       <Header />
@@ -14,4 +14,4 @@ function Page() {
     </div>
   );
 }
-ReactDOM.render(<Page />, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root"));
