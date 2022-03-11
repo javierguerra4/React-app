759d49447c875877ac388b2f47f8d061b9edc024
Javier Guerra <javier.j.guerra4@gmail.com>
2022-03-11 13:15:39 -0600

    Main content color changed, h1 added and color changed

--- src/MainContent.js
+++ src/MainContent.js
@@ -2,14 +2,8 @@ import React from "react";
 
 export default function MainContent() {
   return (
-    <div>
-      <h1>Reasons I'm excited to learn React</h1>
-      <ol>
-        <li>
-          It's a popular library, so I'll be able to fit in with the cool kids!
-        </li>
-        <li>I'm more likely to get a job as a developer if I know React</li>
-      </ol>
+    <div className="main-content">
+      <h1 className="h1-title">Fun facts about React</h1>
     </div>
   );
 }
