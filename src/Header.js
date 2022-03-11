3261adb7b5bdb0d27f598d53493e6430f0e41fb3
Javier Guerra <javier.j.guerra4@gmail.com>
2022-03-10 20:23:59 -0600

    removed div from nav bar

--- src/Header.js
+++ src/Header.js
@@ -4,10 +4,8 @@ export default function Header() {
   return (
     <header className="header-items">
       <nav className="nav">
-        <div className="logo-text">
-          <img className="logo" alt="Logo" src={logo} />
-          <h1 className="react-facts">ReactFacts</h1>
-        </div>
+        <img className="logo" alt="Logo" src={logo} />
+        <h1 className="react-facts">ReactFacts</h1>
         <h1 className="extraTitle">React Course - Project 1</h1>
       </nav>
     </header>
